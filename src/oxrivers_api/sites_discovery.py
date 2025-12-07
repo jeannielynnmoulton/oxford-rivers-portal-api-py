d9e20f35868f6225706f6619aeaa06014596ad14
jeannielynnmoulton <jeannielynnmoulton@gmail..com>
2025-12-07 09:40:06 +0000

    Implement distance from reference point in sites.

--- src/oxrivers_api/sites_discovery.py
+++ src/oxrivers_api/sites_discovery.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import shapely
+from shapely import Point
 
 from src.oxrivers_api.client import OxfordRiversClient
 from src.oxrivers_api.datasets_discovery import DatasetsDiscovery
 from src.oxrivers_api.loader import Loader
+import geopandas as gpd
 
 
 class SitesDiscovery:
@@ -10,6 +13,10 @@ class SitesDiscovery:
     loader: Loader
     datasetID: str
     sites: pd.DataFrame
+
+    crs_meters = "EPSG:27700"  # British National Grid (BNG)
+    crs_lat_lon = "EPSG:4326"  # Standard Latitude and Longitude
+
 
     def __init__(self, loader, datasetID):
         self.loader = loader
@@ -32,15 +39,36 @@ class SitesDiscovery:
         print(result)
         return result
 
+    def get_closest_sites_to(self, lon_lat: tuple[float, float] , num: int=10):
+        lon, lat = lon_lat
+        gdf_sites = gpd.GeoDataFrame(
+            self.sites,
+            geometry=gpd.points_from_xy(self.sites["lon"], self.sites["lat"]),
+            crs=self.crs_lat_lon
+        ).to_crs(self.crs_meters)
+        ref_point = (
+            gpd.GeoSeries([Point(lon, lat)], crs=self.crs_lat_lon)
+            .to_crs(self.crs_meters)
+            .iloc[0]
+        )
+        distance_col = "distance_from_ref"
+        gdf_sites[distance_col] = gdf_sites.geometry.distance(ref_point)
+        return_columns = self.get_columns()
+        return_columns.append(distance_col)
+        return gdf_sites.sort_values(distance_col)[return_columns].head(num)
+
 if __name__ == '__main__':
     client = OxfordRiversClient("../../data")
     loader = Loader(client)
     dataset_discovery = DatasetsDiscovery(loader)
     datasets = dataset_discovery.get_ids()
     site_discovery_list = []
+
     for datasetID in datasets:
-        print(datasetID)
+        # print(datasetID)
         site_discovery = SitesDiscovery(loader, datasetID)
-        info = site_discovery.get_sites_info()
-        site_discovery_list.append(info)
-        print(info.to_string())
+        site_discovery.get_columns()
+        print(site_discovery.get_closest_sites_to((-0.55, 51.4)).to_string())
+        # info = site_discovery.get_sites_info()
+        # site_discovery_list.append(info)
+        # print(info.to_string())
